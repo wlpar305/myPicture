62869188fb31bb237d4a96dc538e3989f47c3d96
wlpar305 <2942932039@qq.com>
2025-02-01 11:01:33 +0800

    create project

---
+++ src/test/java/com/wlc/picture/PictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlc.picture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
